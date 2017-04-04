8acbb7cedcb25b73d054a23843544f7ae35db005
billy <wewill147@gmail.com>
2017-04-04 00:19:34 -0500

    add custom toast for login screen

--- app/src/main/java/com/bedubs/rxfiller/LoginActivity.java
+++ app/src/main/java/com/bedubs/rxfiller/LoginActivity.java
@@ -3,20 +3,26 @@ package com.bedubs.rxfiller;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText edt;
+    TextView toastText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         edt = (EditText)findViewById(R.id.wnum);
+
     }
 
     public void login(View view) {
@@ -28,7 +34,18 @@ public class LoginActivity extends AppCompatActivity {
             Intent intent = new Intent(this, PatientActivity.class);
             startActivity(intent);
         } else {
-            Toast.makeText(getApplicationContext(), "You must enter 7 digits to login." + input, Toast.LENGTH_LONG).show();
+            LayoutInflater inflater = getLayoutInflater();
+            View layout = inflater.inflate(R.layout.custom_toast,
+                    (ViewGroup) findViewById(R.id.custom_toast_container));
+
+            toastText = (TextView) layout.findViewById(R.id.toast_text);
+            toastText.setText("You must enter 7 digits to login.");
+
+            Toast toast = new Toast(getApplicationContext());
+            toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+            toast.setDuration(Toast.LENGTH_LONG);
+            toast.setView(layout);
+            toast.show();
         }
     }
 
